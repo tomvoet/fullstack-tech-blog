b97dd45260d9eb2c011a4f7d8ba58fb8cbd0d07b
Tom Voet <tomvoet@gmx.de>
2022-05-02 17:42:01 +0200

    Database schema update

--- backend/db.setup.sql
+++ backend/db.setup.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP NOT NULL,
     last_login TIMESTAMP,
     intro TEXT
-)
+);
 
 CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
@@ -17,23 +17,28 @@ CREATE TABLE IF NOT EXISTS posts (
     content TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
     views INTEGER NOT NULL DEFAULT 0,
-    FOREIGN KEY (author) REFERENCES users(id)
-)
+	author_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
 
 CREATE TABLE IF NOT EXISTS tags (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS posts_tags (
+	posts_id INT NOT NULL,
+	tags_id INT NOT NULL,
     FOREIGN KEY (posts_id) REFERENCES posts(id),
     FOREIGN KEY (tags_id) REFERENCES tags(id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY,
-    FOREIGN KEY (author) REFERENCES users(id)
-    FOREIGN KEY (post) REFERENCES posts(id)
+	author_id INT NOT NULL,
+	post_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
     created_at TIMESTAMP NOT NULL,
     content TEXT NOT NULL
-)
+);
