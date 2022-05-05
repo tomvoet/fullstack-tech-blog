b221c7bcbac1ae54b124dabee35f4545834a5389
Tom Voet <tomvoet@gmx.de>
2022-05-05 20:18:24 +0200

    Added cors

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const graphql = require('graphql')
+const cors = require('cors')
 
 const schema = require('./services/schema.js').schema;
 
 const app = express();
+
+app.use(cors({origin: '*'}))
 
 app.use('/api', graphqlHTTP({
   schema: schema,
