02c538ee84a675ee421dbc1ca39a6fe3e9942c50
Tom Voet <tomvoet@gmx.de>
2022-05-05 20:18:38 +0200

    Search for user

--- backend/services/resolver.js
+++ backend/services/resolver.js
@@ -13,5 +13,15 @@ exports.resolver = {
         return joinMonster.default(resolveInfo, {}, sql => {
           return psql.query(sql)
         })
-    }
+    },
+    async user(parent, args, context, resolveInfo) {
+        if(args.username) {
+          const result = await psql.query(`SELECT * FROM users WHERE username = '${args.username}'`);
+          return result[0];
+        } else if(args.id) {
+          const result = await psql.query(`SELECT * FROM users WHERE id = ${args.id}`);
+          return result[0];
+        }
+        return null
+      },
 };
