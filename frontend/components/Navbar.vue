f749d91a316ac2a145fb17c54fce0ba06b46a363
Tom Voet <tomvoet@gmx.de>
2022-05-06 21:05:03 +0200

    Navbar struggle

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -2,6 +2,23 @@
   <nav class="bg-gray-800">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 text-gray-400">
       <div class="relative flex items-center justify-between h-16">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <button type="button" @click="toggleMobileMenu()"
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            aria-controls="mobile-menu" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+              stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+        <!-- Mobile menu button end -->
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex-shrink-0 flex items-center">
             <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
@@ -11,15 +28,11 @@
           </div>
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
-              <NuxtLink
-                :class="text - gray - 300 hover: bg - gray - 700 hover: text - white px - 3 py - 2 rounded - md text - sm font - medium"
-                to="/">Home</NuxtLink>
-              <NuxtLink
-                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                to="/posts">Posts</NuxtLink>
-              <NuxtLink
-                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                to="/users">Users</NuxtLink>
+              <NuxtLink :class="$nuxt.$route.path == '/' ? activeButton : inactiveButton" to="/">Home</NuxtLink>
+              <NuxtLink :class="$nuxt.$route.path == '/posts' ? activeButton : inactiveButton" to="/posts">Posts
+              </NuxtLink>
+              <NuxtLink :class="$nuxt.$route.path == '/users' ? activeButton : inactiveButton" to="/users">Users
+              </NuxtLink>
             </div>
           </div>
         </div>
@@ -32,24 +45,83 @@
             <div>
               <button type="button"
                 class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                id="user-menu-button" aria-expanded="false" aria-haspopup="true" @click="toggleUserMenu()">
                 <span class="sr-only">Open user menu</span>
                 <img class="h-8 w-8 rounded-full"
                   src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt="">
               </button>
             </div>
+            <div id="userMenu"
+              class="hidden origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                id="user-menu-item-0">Your Profile</a>
+              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                id="user-menu-item-1">Settings</a>
+              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                id="user-menu-item-2">Sign out</a>
+            </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="sm:hidden hidden" id="mobile-menu">
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        <NuxtLink :class="$nuxt.$route.path == '/' ? activeMobileButton : inactiveMobileButton" to="/">Home</NuxtLink>
+        <NuxtLink :class="$nuxt.$route.path == '/posts' ? activeMobileButton : inactiveMobileButton" to="/posts">Posts
+        </NuxtLink>
+        <NuxtLink :class="$nuxt.$route.path == '/users' ? activeMobileButton : inactiveMobileButton" to="/users">Users
+        </NuxtLink>
       </div>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
-export default ({
+export default {
   name: 'Navbar',
-})
+  data() {
+    return {
+      inactiveButton: "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+      activeButton: "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium",
+      inactiveMobileButton: "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+      activeMobileButton: "bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+    }
+  },
+  methods: {
+    toggleUserMenu() {
+      if (!document.getElementById("userMenu").classList.contains("hidden")) { // leaving
+        document.getElementById("userMenu").classList.add(...["transition", "ease-in", "duration-75"]);
+        document.getElementById("userMenu").classList.add(...["transform", "opacity-100", "scale-100"]);
+        setTimeout(() => {
+          document.getElementById("userMenu").classList.remove(...["transform", "opacity-100", "scale-100"]);
+          document.getElementById("userMenu").classList.add(...["transform", "opacity-0", "scale-95"]);
+          setTimeout(() => {
+            document.getElementById("userMenu").classList.add("hidden");
+            document.getElementById("userMenu").classList.remove(...["transition", "ease-in", "duration-75"]);
+            document.getElementById("userMenu").classList.remove(...["transform", "opacity-0", "scale-95"]);
+          }, 100)
+        }, 100)
+      } else {//entering
+        document.getElementById("userMenu").classList.remove("hidden");
+        document.getElementById("userMenu").classList.add(...["transition", "ease-out", "duration-100"]);
+        document.getElementById("userMenu").classList.add(...["transform", "opacity-0", "scale-95"]);
+        setTimeout(() => {
+          document.getElementById("userMenu").classList.remove(...["transform", "opacity-0", "scale-95"]);
+          document.getElementById("userMenu").classList.add(...["transform", "opacity-100", "scale-100"]);
+          setTimeout(() => {
+            document.getElementById("userMenu").classList.remove(...["transition", "ease-out", "duration-100"]);
+            document.getElementById("userMenu").classList.remove(...["transform", "opacity-100", "scale-100"]);
+          }, 100)
+        }, 100)
+      }
+    },
+    toggleMobileMenu() {
+      document.getElementById("mobile-menu").classList.toggle("hidden")
+    }
+  }
+};
 </script>
 
 
